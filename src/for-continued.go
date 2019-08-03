fbb1f4a5d3cd3edd4a272a07913c37cbfe47b311
chaitu <chaithanyakumar.ds@gmail.com>
2019-08-03 17:18:02 +0530

    loops and conditional statements

---
+++ src/for-continued.go
@@ -0,0 +1,21 @@
+//  the init and post statements are optional
+//  also for is go's while loop
+
+package main
+
+import "fmt"
+
+func main() {
+	sum := 1
+	for sum < 1000 { // linter doe not alloe mw to put ; before and after the condition statement
+		sum += sum
+	}
+	fmt.Println(sum)
+}
+
+//  forever loop
+
+// func main() {
+// 	for {
+// 	}
+// }
